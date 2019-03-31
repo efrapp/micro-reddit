eaa1c8592747e572b4d257dcc7679793be52d484
Efrain Pinto <efrapp@gmail.com>
2019-03-31 10:30:25 -0500

    Aligning hash parameters in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, length: { maximum: 255 },
-            uniqueness: { case_sensitive: false },
-            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+                    uniqueness: { case_sensitive: false },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   validates :username, presence: true, length: { minimum: 5 },
-            uniqueness: { case_sensitives: false }
+                       uniqueness: { case_sensitives: false }
 end
