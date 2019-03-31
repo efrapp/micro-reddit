848ced63b4fa98de48f5bb8b4235e2fb539b4130
Efrain Pinto <efrapp@gmail.com>
2019-03-31 10:47:13 -0500

    Adding freeze to avoid mutability of constant

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   
-  EMAIL_FORMAT_VALIDATION = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  EMAIL_FMT_VALID = (/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i).freeze
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false },
-                    format: { with: EMAIL_FORMAT_VALIDATION.freeze }
+                    format: { with: EMAIL_FMT_VALID }
   validates :username, presence: true, length: { minimum: 5 },
                        uniqueness: { case_sensitives: false }
 end
