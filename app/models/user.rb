aa5e6c6eb287bff18cb89dcd0be19244605379ba
Efrain Pinto <efrapp@gmail.com>
2019-03-31 10:33:43 -0500

    Reducing length of email validation line

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   
+  EMAIL_FORMAT_VALIATION = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+                    format: { with: EMAIL_FORMAT_VALIATION }
   validates :username, presence: true, length: { minimum: 5 },
                        uniqueness: { case_sensitives: false }
 end
